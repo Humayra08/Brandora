73c3bd0653c312f6843d3c8116cfd11544355125
AsmitaEsha <asmitaesha10@gmail.com>
2026-08-29 05:23:15 +0600

    added message option from brand side

--- Brandora.Web/Models/Campaigns/CampaignDetailViewModel.cs
+++ Brandora.Web/Models/Campaigns/CampaignDetailViewModel.cs
@@ -6,4 +6,6 @@ public class CampaignDetailViewModel
 {
     public Campaign Campaign { get; set; } = null!;
     public int ApplicantCount { get; set; }
+    public int CollaborationCount { get; set; }
+    public int ConversationCount { get; set; }
 }
